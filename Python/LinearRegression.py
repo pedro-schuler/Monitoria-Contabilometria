03eb222c5c2e28dc2413f64bd9f60f89760d09ec
Pedro Schuler <schuler.p.m@gmail.com>
2025-03-09 16:31:06 -0300

    Adiciona todas as informações ao modelo de regressão linear

--- Python/LinearRegression.py
+++ Python/LinearRegression.py
@@ -8,9 +8,12 @@ loglin = True
 
 # Arredondamento
 # Define a precisão do arredondamento ao calcular B0 e B1
-precision = 3
+precision = 5
 # Define a precisão do arredondamento ao calcular logaritmos
 lnprecision = 3
+
+# Teste de Hipótese
+hipoteseNula = 0
 
 # nome das variáveis
 VariavelDependente = "Preço da Ação"
@@ -37,6 +40,11 @@ if loglin:
 
 # Cria um DataFrame com os dados
 dadosDataFrame = pd.DataFrame(data=dados_brutos)
+print("Dados iniciais:")
+print(dadosDataFrame)
+print("-------------\n")
+
+# Adiciona colunas para X*Y e X^2
 multiplyXY = dadosDataFrame[VariavelDependente].multiply(
     dadosDataFrame[VariavelIndependente]
 )
@@ -44,7 +52,7 @@ xSquared = dadosDataFrame[VariavelIndependente].pow(2)
 dadosDataFrame["X*Y"] = multiplyXY
 dadosDataFrame["X^2"] = xSquared
 
-# Adiciona uma linha com a soma de todas as colunas numéricas
+# Exibe os dados
 print(dadosDataFrame)
 print("-------------\n")
 
@@ -87,6 +95,72 @@ B0 = np.round(
 
 print("B0 =")
 print(B0)
+print("-------------\n")
+
+# Exibe o modelo calculado
+print("Modelo Calculado:")
+print(f"{VariavelDependente} = {B0} + {B1} * {VariavelIndependente}\n")
+print("-------------\n")
+
+# Calcula todos os Y's
+dadosDataFrame["Y Calculado"] = np.round(
+    dadosDataFrame[VariavelIndependente].apply(lambda x: x * B1 + B0), precision
+)
+
+# Calcula o quadrado resíduos
+dadosDataFrame["U^2"] = np.round(
+    (dadosDataFrame[VariavelDependente] - dadosDataFrame["Y Calculado"]) ** 2, precision
+)
+
+# Calcula o quadrado total
+dadosDataFrame["(Y - Ybarra)^2"] = np.round(
+    (dadosDataFrame[VariavelDependente] - dadosDataFrame[VariavelDependente].mean())
+    ** 2,
+    precision,
+)
+
+# Exibe os dados
+print(dadosDataFrame)
+print("-------------\n")
+
+# Calcula o coeficiente de determinação
+print("Coeficiente de Determinação =")
+SQR = np.round(dadosDataFrame["U^2"].sum(), precision)
+SQT = np.round(dadosDataFrame["(Y - Ybarra)^2"].sum(), precision)
+R2 = np.round(1 - SQR / SQT, precision)
+print(f"SQR = {SQR}")
+print(f"SQT = {SQT}")
+print(f"R2 = {R2}")
+print("-------------\n")
+
+# Calcula o R^2 ajustado
+R2ajustado = np.round(
+    1 - (SQR / (number_of_elements - 1)) / (SQT / (number_of_elements - 1)), precision
+)
+print(f"R^2 Ajustado = {R2ajustado}")
+print("-------------\n")
+
+# Teste de hipótese
+S2 = SQR / (number_of_elements - 2)
+dadosDataFrame["somatorioQuadradoX"] = np.round(
+    (dadosDataFrame[VariavelIndependente] - dadosDataFrame[VariavelIndependente].mean())
+    ** 2,
+    precision,
+)
+
+# Calcula o teste T
+print("Teste T(calculado) =")
+TC = np.round(
+    (B1 - hipoteseNula) / np.sqrt(S2 / dadosDataFrame["somatorioQuadradoX"].sum()),
+    precision,
+)
+print(f"Tc = {TC}")
+print("-------------\n")
+
+# Calcula o Teste F
+print("Teste F(calculado) =")
+FC = np.round(TC**2, precision)
+print(f"Fc = Tc^2 = {FC}")
 print("-------------\n")
 
 # Utilizando o statsmodels
